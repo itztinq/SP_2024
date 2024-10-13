c95b5413643617ff39dc8d9be87b66f3cb8c3ecc
Martina Stojanovska <152183449+itztinq@users.noreply.github.com>
2024-10-13 14:36:17 +0200

    Delete aud1 directory

--- aud1/zadacha1.cpp
+++
@@ -1,8 +0,0 @@
-// programa koja kje ja presmeta vrednosta na matematichkiot izraz: x=3/2+(5–46*5/12)
-#include<iostream>
-using namespace std;
-int main () {
-    float x=3.0/2+(5-46*5.0/12);
-    cout<<"Vrednosta na izrazot e: "<<x<<endl;
-    return 0;
-}
--- aud1/zadacha2.cpp
+++
@@ -1,9 +0,0 @@
-// programa koja za zadadena vrednost na x presmetuva kvadratna vrednost
-
-#include <iostream>
-using namespace std;
-int main () {
-    int x=7;
-    cout<<"Kvadratna vrednost na brojot "<<x<<" e "<<x*x<<endl;
-    return 0;
-}
